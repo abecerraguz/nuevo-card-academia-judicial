b13d28e92309565734fd02dfc30809d22909fa0e
abecerraguz <alejandrobecerrabecerra73@gmail.com>
2023-11-04 09:58:31 -0300

    Se limpia el css

--- src/main.js
+++ src/main.js
@@ -4,21 +4,20 @@ import * as UI from './components/interfaz.js'
 
 const windowObject = window.document;
 
-const buttonCardAcademiaAll = document.querySelectorAll('.cardAcademia__buttonResumen');
-const closeButtonAll = document.querySelectorAll('.cardAcademia__close');
+
 
 
 if( typeof window === 'object' ){
 
     window.addEventListener('DOMContentLoaded', function(){
 
-        console.log('Salida de allbutton-->',buttonCardAcademiaAll)
+        const buttonCardAcademiaAll = document.querySelectorAll('.cardAcademia__buttonResumen');
+        const closeButtonAll = document.querySelectorAll('.cardAcademia__close');
 
         buttonCardAcademiaAll.forEach((element)=>{
             element.addEventListener('click',(e)=>{
                 e.preventDefault();
                 let id = e.target.id.split("-")[1];
-                console.log('Salida de id--->',id)
                 document.querySelector(`#cardAcademia__content-${id}`).classList.add('d-none')
                 document.querySelector(`#cardAcademia__resumen-${id}`).classList.remove('d-none')
                 document.querySelector(`#cardAcademia__close-${id}`).classList.remove('d-none')
@@ -31,7 +30,6 @@ if( typeof window === 'object' ){
                 e.preventDefault();
 
                 let id = e.target.id.split("-")[1];
-                console.log('Salida de id closeButton--->',id)
     
                 document.querySelector(`#cardAcademia__resumen-${id}`).classList.add('d-none')
                 document.querySelector(`#cardAcademia__content-${id}`).classList.remove('d-none')
@@ -43,28 +41,6 @@ if( typeof window === 'object' ){
             })
         })
 
-
-        // console.log('salida de closeButton', closeButton )
-        // closeButton.addEventListener('click',(e)=>{
-
-        //     e.preventDefault()
-     
-        //     let id = e.target.id.split("-")[1];
-        //     console.log('Salida de id closeButton--->',id)
-
-        //     document.querySelector(`#cardAcademia__resumen-${id}`).classList.add('d-none')
-        //     document.querySelector(`#cardAcademia__content-${id}`).classList.remove('d-none')
-        //     document.querySelector(`#cardAcademia__content-${id}`).classList.add('d-block')
-
-        //     document.querySelector(`#cardAcademia__close-${id}`).classList.remove('d-block')
-        //     document.querySelector(`#cardAcademia__close-${id}`).classList.add('d-none')
-        // })
-
-
-
-        // loadPush(windowObject);
-        // UI.buttonSiPorNotificaciones.addEventListener('click', unLoadPush );
-        // UI.buttonNoNotificaciones.addEventListener('click', unLoadPush )
     })
 
 }
